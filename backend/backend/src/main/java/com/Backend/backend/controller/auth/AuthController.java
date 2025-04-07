a9bd6d5c0b2f242bc611f34725ec9436aff38085
tharu-thennakoon <tharushikalpani2003@gmail.com>
2025-04-07 14:24:04 +0530

    Implement login functionality with JWT token generation and error handling

--- backend/backend/src/main/java/com/Backend/backend/controller/auth/AuthController.java
+++ backend/backend/src/main/java/com/Backend/backend/controller/auth/AuthController.java
@@ -1,8 +1,10 @@
 package com.Backend.backend.controller.auth;
 
+import com.Backend.backend.dto.AuthenticationRequest;
 import com.Backend.backend.dto.AuthenticationResponse;
 import com.Backend.backend.dto.SignUprequest;
 import com.Backend.backend.dto.UserDto;
+import com.Backend.backend.entity.UserEntity;
 import com.Backend.backend.repository.UserRepository;
 import com.Backend.backend.services.auth.AuthService;
 import com.Backend.backend.services.jwt.UserService;
@@ -10,10 +12,15 @@ import com.Backend.backend.utils.JWTUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -49,6 +56,23 @@ public class AuthController {
 
     }
 
-    public AuthenticationResponse login(@RequestBody)
-
+    @PostMapping("/login")
+    public AuthenticationResponse login(@RequestBody AuthenticationRequest authenticationRequest) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        final UserDetails userDetails = userService.userDetailService().loadUserByUsername(authenticationRequest.getEmail());
+        Optional<UserEntity> optionalUser = userRepository.findFirstByEmail(authenticationRequest.getEmail());
+        final String jwtToken = jwtUtil.genarateToken(userDetails);
+        AuthenticationResponse authenticationResponse = new AuthenticationResponse();
+        if (optionalUser.isEmpty()) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        authenticationResponse.setJwt(jwtToken);
+        authenticationResponse.setUserId(optionalUser.get().getId());
+        authenticationResponse.setUserRole(optionalUser.get().getUserRole());
+        return authenticationResponse;
+    }
 }
